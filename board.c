2351f36465556f9118d65fe099d181e052dab4c7
Jay Van Dam <vandajay@mail.gvsu.edu>
2020-04-17 16:09:28 -0400

    Getting started analyzing neighbors in update_board

--- board.c
+++ board.c
@@ -17,6 +17,12 @@ int set_dim(Board *b) {
         }
         printf("x=%d y=%d\n", b->nrows, b->ncols);
     }
+    
+    b->grid = (char *)malloc(b->nrows * b->ncols * sizeof(char));
+
+    for(int i=0; i < b->nrows * b->ncols; i++) {
+		*(b->grid+i) = i;
+    }
 
 	printf("SET DIM END\n");
     return q;
@@ -43,5 +49,139 @@ int initialize_board(Board *b) {
     return q;
 }
 
-//void update_board(Board *b) { // Better NOT to use prototypes for blank functions...
-//}
+void update_board(Board *b) { // Better NOT to use prototypes for blank functions...
+    printf("**updating board**\n");
+
+    char temp[b->nrows][b->ncols];
+
+    for(int r = 0; r < b->nrows; r++) {
+        for(int c = 0; c < b->ncols; c++) {
+            temp[r][c] = '.';
+        }
+    }
+
+    for(int r = 0; r < b->nrows; r++) {
+        for(int c = 0; c < b->ncols; c++) {
+            printf("[%d][%d]\n", r, c);
+            int wcnt = 0;
+            int bcnt = 0;
+            int xcnt = 0; 
+
+            // Count types surrounding position 
+            if(*(b->grid + ((r-1) % b->nrows) * b->ncols + ((c-1) % b->ncols)) == 'w') {
+                printf("%c", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r-1) % b->nrows) * b->ncols + ((c-1) % b->ncols)) == 'b') {
+                printf("%c", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                xcnt++;
+            }
+
+            if(*(b->grid + ((r-1) % b->nrows) * b->ncols + ((c) % b->ncols)) == 'w') {
+                printf("%c", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r-1) % b->nrows) * b->ncols + ((c) % b->ncols)) == 'b') {
+                printf("%c", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c) % b->ncols)));
+                xcnt++;
+            }
+
+            if(*(b->grid + ((r-1) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'w') {
+                printf("%c,", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r-1) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'b') {
+                printf("%c,", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c,", *(b->grid + ((r-1) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                xcnt++;
+            }
+
+            if(*(b->grid + ((r) % b->nrows) * b->ncols + ((c-1) % b->ncols)) == 'w') {
+                printf("%c", *(b->grid + ((r) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'b') {
+                printf("%c", *(b->grid + ((r) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c", *(b->grid + ((r) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                xcnt++;
+            }
+
+            if(*(b->grid + ((r) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'w') {
+                printf("%c,", *(b->grid + ((r) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'b') {
+                printf("%c,", *(b->grid + ((r) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c,", *(b->grid + ((r) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                xcnt++;
+            }
+
+
+            if(*(b->grid + ((r+1) % b->nrows) * b->ncols + ((c-1) % b->ncols)) == 'w') {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r+1) % b->nrows) * b->ncols + ((c-1) % b->ncols)) == 'b') {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c-1) % b->ncols)));
+                xcnt++;
+            }
+
+            if(*(b->grid + ((r+1) % b->nrows) * b->ncols + ((c) % b->ncols)) == 'w') {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r+1) % b->nrows) * b->ncols + ((c) % b->ncols)) == 'b') {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c) % b->ncols)));
+                xcnt++;
+            }
+
+            if(*(b->grid + ((r+1) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'w') {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                wcnt++;
+            } else if (*(b->grid + ((r+1) % b->nrows) * b->ncols + ((c+1) % b->ncols)) == 'b') {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                bcnt++;
+            } else {
+                printf("%c", *(b->grid + ((r+1) % b->nrows) * b->ncols + ((c+1) % b->ncols)));
+                xcnt++;
+            }
+
+            printf("\nw[%d] b[%d] x[%d]\n", wcnt, bcnt, xcnt);
+
+            // Check for Overpopulation for white and blue
+            if(*(b->grid + r * b->ncols + c) == 'w' || *(b->grid + r * b->ncols + c) == 'b') {
+                if((wcnt+bcnt) > 3) {
+                    temp[r][c] = 'x';
+                } else if((wcnt+bcnt) < 2) {
+                    temp[r][c] = 'x';
+                } else {
+                    temp[r][c] = *(b->grid + r * b->ncols + c);
+                }
+            } else if (*(b->grid + r * b->ncols + c) == 'x') { // Check for Birth
+                if (wcnt == 3) {
+                    temp[r][c] = 'w';
+                } else if (bcnt == 3) {
+                    temp[r][c] = 'b';
+                } else {
+                    temp[r][c] = 'x';
+                }
+            }
+            printf("%c->%c\n", *(b->grid + r * b->ncols + c), temp[r][c]);
+        }
+    }
+    printf("grid=%s\n", b->grid);
+    printf("temp=%s\n", *temp);
+
+    printf("**END updating board**\n");
+}
--- conway.c
+++ conway.c
@@ -20,46 +20,35 @@ int main(void) {
 
     do {
         printf("\nBEGIN LOOP\n");
+
         q = set_dim(bptr);
-        printf("q=%d\n", q);
         printf("rows=%d cols=%d\n", bptr->nrows, bptr->ncols);
 
-        bptr->grid = (char *)malloc(bptr->nrows * bptr->ncols * sizeof(char));
 
-	    for(int i=0; i < bptr->nrows * bptr->ncols; i++) {
-		    *(bptr->grid+i) = i;
-    	}
-
-        printf("EOF=%d\n", EOF);
         if (q != EOF) {
             q = scanf("%d", &steps);
+	        printf("steps=%d\n", steps);
         }
 
-	    printf("steps=%d\n", steps);
-	    
-		q = initialize_board(bptr);
+	    if (q != EOF) {
+		    q = initialize_board(bptr);
+            printf("grid=%s\n", bptr->grid);
+        }
 
-        printf("%s\n", bptr->grid);
+        if (q != EOF && steps != 0) {
+		    for(int i = 0; i < steps; i++)
+                update_board(bptr);
+        } 
+        else {
+            q = EOF;
+        }
 
-//        if (q != EOF) {
-//            if (steps != 0) {
-//                printf("**updating board**\n");
-//		        for(int i = 0; i < steps; i++) {
-//                    update_board(bptr);
-//                }
-//            } else {
-//                q = EOF;
-//            }
-//        }
+        if (q != EOF) {
+            q = count_colors(bptr);
+        }
 
-//        if (q != EOF) {
-//            q = count_colors(bptr);
-//        }
        printf("\nEND LOOP\n"); 
     } while (q != EOF);
-
-
-
 
     free(bptr);
     free(bptr->grid);
